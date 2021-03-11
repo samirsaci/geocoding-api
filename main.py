539a92a056d5b471227d770ab360a694a47f1912
Samir <s.saci95@gmail.com>
2021-03-11 21:44:53 +0100

    second commit

--- main.py
+++ main.py
@@ -4,17 +4,36 @@
 # Step 2: Add the PATH variable to the Heroku configuration
 # heroku config:set GOOGLE_CHROME_BIN=/app/.apt/usr/bin/google_chrome
 # heroku config:set CHROMEDRIVER_PATH=/app/.chromedriver/bin/chromedriver
+# heroku ps:scale worker=0 
 from selenium import webdriver
+from bs4 import BeautifulSoup as soup 
 import os
 
+# Chromedrive setting
 chrome_options = webdriver.ChromeOptions()
-
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-driver.get("https://medium.com")
-print(driver.page_source)
+
+# Link
+link = "https://medium.com"
+fr = 'Paris, France'
+to = 'Marseille, France'
+
+# url
+url = 'https://www.google.com/maps/dir/{}/{}'.format(fr, to)
+# Driver get
+driver.get(url)
+# Get pagesoup
+page_soup = soup(driver.page_source)
+# Extract
+css_dist = "div[class^='section-directions-trip-distance'] > div"
+try:
+    distance = page_soup.select_one(css_dist).text
+except:
+    distance = 'Error'
+
+print("{} to {}: {}".format(fr, to, distance))
 print("Finished!")
