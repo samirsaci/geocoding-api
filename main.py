6a4330e1dee0d847c32b55524936941959f3402b
Samir <s.saci95@gmail.com>
2021-03-11 21:07:28 +0100

    first commit

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+# Step 1: Download the Buildpacks Necessary for the ChromeDriver
+# heroku buildpacks:add --index 1 https://github.com/heroku-buildpack-chromedriver
+# heroku buildpacks:add --index 2 https://github.com/heroku-buildpack-chromedriver
+# Step 2: Add the PATH variable to the Heroku configuration
+# heroku config:set GOOGLE_CHROME_BIN=/app/.apt/usr/bin/google_chrome
+# heroku config:set CHROMEDRIVER_PATH=/app/.chromedriver/bin/chromedriver
 from selenium import webdriver
 import os
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-driver = webdriver.Chrome(executable_path = os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-driver.get("https://medium.com")
+GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 
-print(driver.page_source)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.binary_location = GOOGLE_CHROME_PATH
+browser = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+browser.get("https://medium.com")
+
+print(browser.page_source)
 print("Finished!")
