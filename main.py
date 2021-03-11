12ed1ef81ab1f808e38ff2dfc336921b090e8cab
Samir <s.saci95@gmail.com>
2021-03-11 22:57:49 +0100

    second commit

--- main.py
+++ main.py
@@ -26,13 +26,11 @@ to = 'Marseille, France'
 # url
 url = 'https://www.google.fr/maps/dir/{}/{}/data=!4m2!4m1!3e0'.format(fr, to)
 print("link: {}".format(url))
-# url = 'https://www.google.fr/maps/dir/Paris/Marseille/@45.988302,-0.6461436,6z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x47e66e1f06e2b70f:0x40b82c3688c9460!2m2!1d2.3522219!2d48.856614!1m5!1m1!1s0x12c9bf4344da5333:0x40819a5fd970220!2m2!1d5.36978!2d43.296482!3e0'
 # Driver get
 driver.get(url)
 time.sleep(3)
 # Get pagesoup
 page_soup = soup(driver.page_source, "html.parser")
-print(page_soup)
 # Extract
 css_dist = "div[class^='section-directions-trip-distance'] > div"
 try:
