6d75bccedca4b9971390bfe52996884b0217f568
Samir <s.saci95@gmail.com>
2021-03-11 22:09:44 +0100

    second commit

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ to = 'Marseille, France'
 url = 'https://www.google.com/maps/dir/{}/{}'.format(fr, to)
 # Driver get
 driver.get(url)
+print(driver.page_source)
 # Get pagesoup
 page_soup = soup(driver.page_source)
 # Extract
